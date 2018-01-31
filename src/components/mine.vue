66330384087e643fa42c7d9e9e5347c27a795e3a
Guotong666 <512952764@qq.com>
2018-01-31 11:29:52 +0800

    luyou

---
+++ src/components/mine.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+       
+    </div>
+</template>
+    
+<script>
+export default {
+    name: "mine",
+    data () {
+        return {
+             
+        };
+    }
+}
+</script>
+    
+<style lang="css" scoped>
+    
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import mine from '../components/mine.vue'
+import go from '../components/go.vue'
+import fashion from '../components/fashion.vue'
+import home from '../components/home.vue'
+import shop from '../components/shop.vue'
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+    { path: '', component: home},
+    { path: '/home', component: home },
+    { path: '/fashion', component: fashion },
+    { path: '/shop', component: shop },
+    { path: '/go', component: go},
+    { path: '/mine', component: mine }
   ]
 })
